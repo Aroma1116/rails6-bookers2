9cb348c8b0c4d7a50a437eea015910a37f71a38d
EC2 Default User <ec2-user@ip-172-31-39-191.ap-northeast-1.compute.internal>
2023-05-30 00:28:03 +0000

    devise日本語化、パスワード変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,5 +14,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :introduction, :profile_image, :email])
   end
+  #jjan-uxrt-xvnu-veqm
 end
