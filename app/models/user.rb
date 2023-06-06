0530ff24b50882ad7c0f20d6b8096b2e45097819
EC2 Default User <ec2-user@ip-172-31-39-191.ap-northeast-1.compute.internal>
2023-06-06 12:15:06 +0000

    郵便番号から自動で住所の入力完了、残りは地図の表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,27 @@ class User < ApplicationRecord
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: {maximum: 50}
+  validates :postal_code, presence: true
+  validates :address, presence: true
 
+  include JpPrefecture
+  jp_prefecture :prefecture_code
+
+
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+
+  def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
+
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
 
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+
 end
